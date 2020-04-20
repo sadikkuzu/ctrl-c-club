9271aa3d013a9c804dc97b1647017020bb0b0780
SADIK KUZU <sadikkuzu@hotmail.com>
2020-04-20 12:36:48 +0300

    Iris into daily htmls

--- startnewday.sh
+++ startnewday.sh
@@ -3,5 +3,6 @@
 echo "<pre>$(cal -h)</pre><br/>"
 echo '<a href="http://ctrl-c.club/~sadikkuzu/">~sadikkuzu</a>'
 echo '<a href="http://ctrl-c.club/~pgadey/updated.html">last updated pages</a>'
-echo '<a href="https://github.com/sadikkuzu" target="_blank">@sadikkuzu</a><br/>'
+echo '<a href="https://github.com/sadikkuzu/ctrl-c-club/blob/master/startnewday.sh" target="_blank">@sadikkuzu</a><br/>'
+echo "<pre>$(iris -s)</pre><br/>" | sed 's/Iris/<a href="https:\/\/github.com\/Calamitous\/iris" target="_blank">Iris<\/a>/'
 } >> ~/public_html/$(date +%Y-%m-%d).html
