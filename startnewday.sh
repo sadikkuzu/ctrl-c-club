6d1d0869f278bec6be057499a0f423a57d51e1cf
SADIK KUZU <sadikkuzu@hotmail.com>
2020-04-26 21:50:31 +0300

    exact path of iris

--- startnewday.sh
+++ startnewday.sh
@@ -4,5 +4,5 @@ echo "<pre>$(cal -h)</pre><br/>"
 echo '<a href="http://ctrl-c.club/~sadikkuzu/">~sadikkuzu</a>'
 echo '<a href="http://ctrl-c.club/~pgadey/updated.html">last updated pages</a>'
 echo '<a href="https://github.com/sadikkuzu/ctrl-c-club/blob/master/startnewday.sh" target="_blank">@sadikkuzu</a><br/>'
-echo "<pre>$(iris -s)</pre><br/>" | sed 's/Iris/<a href="https:\/\/github.com\/Calamitous\/iris" target="_blank">Iris<\/a>/'
+echo "<pre>$(/usr/local/bin/iris -s)</pre><br/>" | sed 's/Iris/<a href="https:\/\/github.com\/Calamitous\/iris" target="_blank">Iris<\/a>/'
 } >> ~/public_html/$(date +%Y-%m-%d).html
