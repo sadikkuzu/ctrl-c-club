2609739c56561579bd4ae45f2e329587b736dbf1
SADIK KUZU <sadikkuzu@hotmail.com>
2021-06-20 02:20:49 +0300

    next(g) and tests

--- socials.py
+++ socials.py
@@ -14,69 +14,14 @@ github = []
 found = []
 
 
-def ayikla(liste):
-    for adres in liste:
+def ayikla(adresler):
+    for adres in adresler:
         try:
             r = requests.get('https://' + adres.split("https://")[-1])
             if r.status_code != 200:
-                liste.remove(adres)
+                adresler.remove(adres)
         except Exception:
-            liste.remove(adres)
-
-
-for filename in os.listdir('/home'):
-
-    s = ''
-    try:
-        s = open('/home/' + filename + '/public_html/index.html', 'r').read()
-    except Exception:
-        continue
-
-    tw = re.findall(r'twitter\.com\/[a-z0-9A-Z_]+', s)
-    ma = re.findall(r'[a-zA-Z0-9\.]+\/@[a-z0-9A-Z_]+', s)
-    ig = re.findall(r'instagram\.com\/[a-z0-9A-Z_]+', s)
-    gh = re.findall(r'https://github\.com\/[a-z0-9A-Z_]+', s)
-
-    ayikla(tw)
-    # ayikla(ma)
-    ayikla(ig)
-    ayikla(gh)
-
-    if len(tw):
-        for item in tw:
-            if item not in found and item != 'twitter.com/widgets':
-                twitter.append({
-                    'url': item,
-                    'username': filename,
-                })
-                found.append(item)
-
-    if len(ma):
-        for item in ma:
-            if item not in found and "medium.com" not in item:
-                mastodon.append({
-                    'url': item,
-                    'username': filename,
-                })
-                found.append(item)
-
-    if len(ig):
-        for item in ig:
-            if item not in found:
-                instagram.append({
-                    'url': item,
-                    'username': filename,
-                })
-                found.append(item)
-
-    if len(gh):
-        for item in gh:
-            if item not in found and '/elm' not in item:
-                github.append({
-                    'url': item.split("https://")[-1],
-                    'username': filename,
-                })
-                found.append(item)
+            adresler.remove(adres)
 
 
 def baloncuksort(liste, balon):
@@ -84,23 +29,16 @@ def baloncuksort(liste, balon):
     g = (item for item in liste if item["username"] == str(balon))
     while True:
         try:
-            l2.append(g.next())
+            l2.append(next(g))
         except Exception:
             break
     g = (item for item in liste if item["username"] != str(balon))
     while True:
         try:
-            l2.append(g.next())
+            l2.append(next(g))
         except Exception:
             break
     return l2
-
-
-bubble = "sadikkuzu"
-github = baloncuksort(github, bubble)
-twitter = baloncuksort(twitter, bubble)
-mastodon = baloncuksort(mastodon, bubble)
-instagram = baloncuksort(instagram, bubble)
 
 
 def olustur(liste):
@@ -117,34 +55,110 @@ def olustur(liste):
     print('<br><br>')
 
 
-print(
-    '<html><body><link href='
-    '"https://fonts.googleapis.com/css?family=Roboto+Mono:300,700" '
-    'rel="stylesheet"><style type="text/css">body {background-color:#1C1C1C;'
-    'color: #fff;font-family: "Roboto Mono", "Hack", "Inconsolata", monospace;'
-    '}a{color:#00AAEE;}</style>',
-)
+def main():
+    global twitter
+    global instagram
+    global mastodon
+    global github
+    global found
 
-print(f"[*] <strong>Updated at :</strong> {str(datetime.datetime.now())}<br>")
+    for filename in os.listdir('/home'):
 
-total_count = len(github) + len(twitter) + len(mastodon) + len(instagram)
-print(f"[*] <strong>Total count:</strong> {total_count}<br><br>")
+        s = ''
+        try:
+            s = open(f'/home/{filename}/public_html/index.html', 'r').read()
+        except Exception:
+            continue
 
-olustur(github)
-olustur(twitter)
-olustur(mastodon)
-olustur(instagram)
+        tw = re.findall(r'twitter\.com\/[a-z0-9A-Z_]+', s)
+        ma = re.findall(r'[a-zA-Z0-9\.]+\/@[a-z0-9A-Z_]+', s)
+        ig = re.findall(r'instagram\.com\/[a-z0-9A-Z_]+', s)
+        gh = re.findall(r'https://github\.com\/[a-z0-9A-Z_]+', s)
 
-print(
-    'This page is inspired by '
-    '<a href="http://ctrl-c.club/~0x00ctrl" target="_blank">~0x00ctrl</a>\'s '
-    '<a href="http://ctrl-c.club/~0x00ctrl/social.html" target="_blank">'
-    'social</a> page.<br/>',
-)
-print(
-    '<a href="https://github.com/sadikkuzu/ctrl-c-club/blob/master/socials.py"'
-    ' target="_blank">Developed</a> by'
-    '<a href="http://ctrl-c.club/~sadikkuzu" target="_blank">~sadikkuzu</a>',
-)
+        ayikla(tw)
+        # ayikla(ma)
+        ayikla(ig)
+        ayikla(gh)
 
-print('</body></html>')
+        if len(tw):
+            for item in tw:
+                if item not in found and item != 'twitter.com/widgets':
+                    twitter.append({
+                        'url': item,
+                        'username': filename,
+                    })
+                    found.append(item)
+
+        if len(ma):
+            for item in ma:
+                if item not in found and "medium.com" not in item:
+                    mastodon.append({
+                        'url': item,
+                        'username': filename,
+                    })
+                    found.append(item)
+
+        if len(ig):
+            for item in ig:
+                if item not in found:
+                    instagram.append({
+                        'url': item,
+                        'username': filename,
+                    })
+                    found.append(item)
+
+        if len(gh):
+            for item in gh:
+                if item not in found and '/elm' not in item:
+                    github.append({
+                        'url': item.split("https://")[-1],
+                        'username': filename,
+                    })
+                    found.append(item)
+
+    bubble = "sadikkuzu"
+    github = baloncuksort(github, bubble)
+    twitter = baloncuksort(twitter, bubble)
+    mastodon = baloncuksort(mastodon, bubble)
+    instagram = baloncuksort(instagram, bubble)
+
+    print(
+        '<html><body><link href="https://'
+        'fonts.googleapis.com/css?family=Roboto+Mono:300,700" rel="style'
+        'sheet"><style type="text/css">body {background-color:#1C1C1C;color: '
+        '#fff;font-family: "Roboto Mono", "Hack", "Inconsolata", monospace;'
+        '}a{color:#00AAEE;}</style>',
+    )
+
+    print(
+        f"[*] <strong>Updated at :</strong> "
+        f"{str(datetime.datetime.now())}<br>"
+    )
+
+    total_count = len(github) + len(twitter) + len(mastodon) + len(instagram)
+    print(f"[*] <strong>Total count:</strong> {total_count}<br><br>")
+
+    olustur(github)
+    olustur(twitter)
+    olustur(mastodon)
+    olustur(instagram)
+
+    print(
+        'This page is inspired by '
+        '<a href="http://ctrl-c.club/~0x00ctrl" target="_blank">'
+        '~0x00ctrl</a>\'s '
+        '<a href="http://ctrl-c.club/~0x00ctrl/social.html" target="_blank">'
+        'social</a> page.<br/>',
+    )
+    print(
+        '<a href="https://github.com/sadikkuzu/ctrl-c-club'
+        '/blob/master/socials.py"'
+        ' target="_blank">Developed</a> by <a '
+        'href="http://ctrl-c.club/~sadikkuzu" target="blank">~sadikkuzu</a>',
+    )
+
+    print('</body></html>')
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_socials.py
@@ -0,0 +1,86 @@
+import pytest
+
+from socials import ayikla
+from socials import baloncuksort as bs
+
+
+@pytest.mark.parametrize(
+    "adresler, expected_adresler",
+    [
+        (
+            ["dsajfhklajhfda.kadjfaj"],
+            [],
+        ),
+        (
+            ["https://twitter.com/sadikkuzu_mba"],
+            ["https://twitter.com/sadikkuzu_mba"],
+        ),
+    ]
+)
+def test_ayikla(adresler, expected_adresler):
+    ayikla(adresler)
+    assert adresler == expected_adresler
+
+
+@pytest.mark.parametrize(
+    "liste, balon, expected_liste",
+    [
+        ([], "", []),
+        ([], "sadikkuzu", []),
+        (
+            [
+                {
+                    'url': "https://twitter.com/dummy",
+                    'username': "dummy"
+                },
+                {
+                    'url': "https://twitter.com/sadikkuzu_mba",
+                    'username': "sadikkuzu"
+                },
+            ],
+            "sadikkuzu",
+            [
+                {
+                    'url': "https://twitter.com/sadikkuzu_mba",
+                    'username': "sadikkuzu"
+                },
+                {
+                    'url': "https://twitter.com/dummy",
+                    'username': "dummy"
+                },
+            ],
+        ),
+        (
+            [
+                {'url': "https://twitter.com/dummy", 'username': "dummy"},
+                {'url': "https://twitter.com/dummy2", 'username': "dummy2"},
+            ],
+            "sadikkuzu",
+            [
+                {'url': "https://twitter.com/dummy", 'username': "dummy"},
+                {'url': "https://twitter.com/dummy2", 'username': "dummy2"},
+            ],
+        ),
+        (
+            [
+                {'url': "https://twitter.com/dummy", 'username': "dummy"},
+                {'url': "https://twitter.com/dummy2", 'username': "dummy2"},
+                {
+                    'url': "https://twitter.com/sadikkuzu_mba",
+                    'username': "sadikkuzu"
+                },
+            ],
+            "sadikkuzu",
+            [
+                {
+                    'url': "https://twitter.com/sadikkuzu_mba",
+                    'username': "sadikkuzu"
+                },
+                {'url': "https://twitter.com/dummy", 'username': "dummy"},
+                {'url': "https://twitter.com/dummy2", 'username': "dummy2"},
+            ],
+        ),
+    ]
+)
+def test_baloncuksort(liste, balon, expected_liste):
+    assert bs(liste, balon) == expected_liste
